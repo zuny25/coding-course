53131e856711d148ba8f87c029082566359c52c5
Haden Kim <haden@glorang.com>
2024-03-13 17:53:24 +0900

    feat user detail init

--- src/app/admin/user/[id]/page.tsx
+++ src/app/admin/user/[id]/page.tsx
@@ -1,3 +1,21 @@
-export default function AdminUserDetailPage() {
-  return <section>AdminUser Detail</section>
+import findUserById from '@/data-access/user/findUserById'
+import { notFound } from 'next/navigation'
+import UserDetail from './_components/UserDetail'
+
+export default async function AdminUserDetailPage({
+  params,
+}: {
+  params: { id: string }
+}) {
+  try {
+    if (!params.id) notFound()
+    const user = await findUserById(params.id)
+    if (!user) {
+      notFound()
+    }
+
+    return <UserDetail user={user} />
+  } catch (_e) {
+    notFound()
+  }
 }
